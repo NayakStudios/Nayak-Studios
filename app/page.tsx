77ccd44e2c859a8d598da13f4f081c043423978b
Nayak Studios <05.nayakstudios@gmail.com>
2026-08-14 13:10:25 +0530

    Initial website commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../component/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        background: "#030617",
+        minHeight: "100vh",
+      }}
+    >
+
+      <Navbar />
+
+    </main>
+  );
+}
